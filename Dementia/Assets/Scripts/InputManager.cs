63f6d5d2f2eff99a690b5b721d7996ba6972c4e2
SadraHeidariMoghadam <sadra_h_m@outlook.com>
2022-07-28 10:41:26 +0430

    Player movement completed except crouching

--- Dementia/Assets/Scripts/InputManager.cs
+++ Dementia/Assets/Scripts/InputManager.cs
@@ -1,50 +1,87 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.HID;
+using UnityEngine.InputSystem.Interactions;
 
 public class InputManager : MonoBehaviour
 {
-    private PlayerControlls playerControlls;
-    private static InputManager _instance;
+    [SerializeField] private PlayerInput PlayerInput;
 
-    public static InputManager Instance
-    {
-        get => _instance;
+        public Vector2 Move {get; private set;}
+        public Vector2 Look {get; private set;}
+        public bool Run {get; private set;}
+        // public bool Jump {get; private set;}
+        // public bool Crouch {get; private set;}
+
+        private InputActionMap _currentMap;
+        private InputAction _moveAction;
+        private InputAction _lookAction;
+        private InputAction _runAction;
+        private InputAction _jumpAction;
+        private InputAction _crouchAction;
+
+        private void Awake()
+        {
+            HideCursor();
+            _currentMap = PlayerInput.currentActionMap;
+            _moveAction = _currentMap.FindAction("Move");
+            _lookAction = _currentMap.FindAction("Look");
+            _runAction = _currentMap.FindAction("Run");
+            _jumpAction = _currentMap.FindAction("Jump");
+            _crouchAction = _currentMap.FindAction("Crouch");
+
+            _moveAction.performed += onMove;
+            _lookAction.performed += onLook;
+            _runAction.performed += onRun;
+            // _jumpAction.performed += onJump;
+            // _crouchAction.started += onCrouch;
+
+            _moveAction.canceled += onMove;
+            _lookAction.canceled += onLook;
+            _runAction.canceled += onRun;
+            // _jumpAction.canceled += onJump;
+            // _crouchAction.canceled += onCrouch;
+        }
+
+        private void HideCursor()
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void onMove(InputAction.CallbackContext context)
+        {
+            Move = context.ReadValue<Vector2>();
+        }
         
-    }
-
-    private void Awake()
-    {
-        if (_instance != null && _instance != this)
+        private void onLook(InputAction.CallbackContext context)
         {
-            Destroy(gameObject);
+            Look = context.ReadValue<Vector2>();
         }
-        else
+        
+        private void onRun(InputAction.CallbackContext context)
         {
-            _instance = this;
+            Run = context.ReadValueAsButton();
         }
-        playerControlls = new PlayerControlls();
-    }
+        
+        // private void onJump(InputAction.CallbackContext context)
+        // {
+        //     Jump = context.ReadValueAsButton();
+        // }
+        
+        // private void onCrouch(InputAction.CallbackContext context)
+        // {
+        //     Crouch = context.ReadValueAsButton();
+        // }
 
-    private void OnEnable()
-    {
-        playerControlls.Enable();
-    }
+        private void OnEnable() 
+        {
+            _currentMap.Enable();
+        }
 
-    private void OnDisable()
-    {
-        playerControlls.Disable();
-    }
-
-    public Vector2 GetPlayerMovement()
-    {
-        return playerControlls.Player.Movement.ReadValue<Vector2>();
-    }
-    
-    public Vector2 GetMouseDelta()
-    {
-        return playerControlls.Player.Look.ReadValue<Vector2>();
-    }
+        private void OnDisable() 
+        {
+            _currentMap.Disable();
+        }
 }
--- Dementia/Assets/Scripts/PlayerController.cs
+++ Dementia/Assets/Scripts/PlayerController.cs
@@ -1,52 +1,110 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 
-[RequireComponent(typeof(CharacterController))]
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float playerSpeed = 2.0f;
-    [SerializeField] private float jumpHeight = 0.0f;
-    private CharacterController controller;
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
-    private float gravityValue = -9.81f;
-    private InputManager inputManager;
-
-    private void Start()
+    [SerializeField] private float animBlendSpeed = 8.9f;
+    [SerializeField] private Transform cameraRoot;
+    [SerializeField] private Transform camera;
+    [SerializeField] private float upperLimit = -40f;
+    [SerializeField] private float bottomLimit = 70f;
+    [SerializeField] private float mouseSensitivity = 10f;
+    private Rigidbody _playerRigidbody;
+    private InputManager _inputManager;
+    private Animator _animator;
+    private bool _grounded = false;
+    private bool _hasAnimator;
+    private int _xVelocityHash;
+    private int _yVelocityHash;
+    private float _xRotation;
+    private const float _walkSpeed = 4f;
+    private const float _runSpeed = 6f;
+    private Vector2 _currentVelocity;
+        
+    private void Start() 
     {
-        controller = GetComponent<CharacterController>();
-        inputManager = InputManager.Instance;
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        _hasAnimator = TryGetComponent<Animator>(out _animator);
+        // _characterController = GetComponent<CharacterController>();
+        _playerRigidbody = GetComponent<Rigidbody>();
+        _inputManager = GetComponent<InputManager>();
+        _xVelocityHash = Animator.StringToHash("XVelocity");
+        _yVelocityHash = Animator.StringToHash("YVelocity");
     }
 
-    void Update()
+    private void FixedUpdate() 
     {
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
-        {
-            playerVelocity.y = 0f;
-        }
-
-        // Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        Vector2 movement = inputManager.GetPlayerMovement();
-        Vector3 move = new Vector3(movement.x, 0, movement.y);
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
-        if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }
-
-        // if (Input.GetButtonDown("Jump") && groundedPlayer)
-        // {
-        //     playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        // }
-
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+        Move();
     }
+    private void LateUpdate()
+    {
+        CameraMovement();
+    }
+    
+    private void Move()
+    {
+        if(!_hasAnimator) 
+            return;
+        float targetSpeed = _inputManager.Run ? _runSpeed : _walkSpeed;
+        if(_inputManager.Move ==Vector2.zero) 
+            targetSpeed = 0;
+        _currentVelocity.x = Mathf.Lerp(_currentVelocity.x, _inputManager.Move.x * targetSpeed, animBlendSpeed * Time.fixedDeltaTime);
+        _currentVelocity.y = Mathf.Lerp(_currentVelocity.y, _inputManager.Move.y * targetSpeed, animBlendSpeed * Time.fixedDeltaTime);
+        float step = targetSpeed * Time.deltaTime;
+        var newPos = new Vector3(_currentVelocity.x, 0, _currentVelocity.y);
+        transform.position += transform.TransformDirection (newPos / 40);
+        _animator.SetFloat(_xVelocityHash, _currentVelocity.x);
+        _animator.SetFloat(_yVelocityHash, _currentVelocity.y);
+    }
+
+    private void CameraMovement()
+    {
+        if(!_hasAnimator) 
+            return;
+        var Mouse_X = _inputManager.Look.x;
+        var Mouse_Y = _inputManager.Look.y;
+        camera.position = cameraRoot.position;
+        _xRotation -= Mouse_Y * mouseSensitivity * Time.smoothDeltaTime;
+        _xRotation = Mathf.Clamp(_xRotation, upperLimit, bottomLimit);
+        camera.localRotation = Quaternion.Euler(_xRotation, 0 , 0);
+        _playerRigidbody.MoveRotation(_playerRigidbody.rotation * Quaternion.Euler(0, Mouse_X * mouseSensitivity * Time.smoothDeltaTime, 0));
+    }
+
+    // private void Move()
+    // {
+    //     if(!_hasAnimator) 
+    //         return;
+    //     float targetSpeed = _inputManager.Run ? _runSpeed : _walkSpeed;
+    //     if (_inputManager.Move == Vector2.zero)
+    //         targetSpeed = 0.1f;
+    //     _currentVelocity.x = Mathf.Lerp(_currentVelocity.x, targetSpeed * _inputManager.Move.x, animationSpeed * Time.fixedDeltaTime);
+    //     _currentVelocity.y = Mathf.Lerp(_currentVelocity.y, targetSpeed * _inputManager.Move.y, animationSpeed * Time.fixedDeltaTime);
+    //
+    //     var xVelocityDifference = _currentVelocity.x - _playerRigidbody.velocity.x;
+    //     var zVelocityDifference = _currentVelocity.y - _playerRigidbody.velocity.z;
+    //     
+    //     _playerRigidbody.AddForce(transform.TransformVector(new Vector3(xVelocityDifference, 0, zVelocityDifference)), ForceMode.VelocityChange);
+    //     
+    //     _animator.SetFloat(_xVelocityHash, _currentVelocity.x);
+    //     _animator.SetFloat(_yVelocityHash, _currentVelocity.y);
+    // }
+    //
+    // private void CameraMovement()
+    // {
+    //     if(!_hasAnimator) 
+    //         return;
+    //     var mouseX = _inputManager.Look.x;
+    //     var mouseY = _inputManager.Look.y;
+    //     camera.position = cameraRoot.position;
+    //     _xRotation -= mouseY * mouseSensitivity * Time.deltaTime;
+    //     _xRotation = Mathf.Clamp(_xRotation, upperLimit, bottomLimit);
+    //     
+    //     camera.localRotation = Quaternion.Euler(_xRotation, 0, 0);
+    //     transform.Rotate(Vector3.up, mouseX * mouseSensitivity * Time.deltaTime);
+    // }
+
+    
 }
