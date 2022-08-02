a3921333439b9275bd1e6c71337bc16bfc7d950b
SadraHeidariMoghadam <sadra_h_m@outlook.com>
2022-08-03 02:08:17 +0430

    Added Enemy On Reach Player Added enemy red light Added new package for map design Added new animation for the mutant

--- Dementia/Assets/Scripts/Enemy/EnemyAI.cs
+++ Dementia/Assets/Scripts/Enemy/EnemyAI.cs
@@ -10,16 +10,18 @@ public class EnemyAI : MonoBehaviour
 {
     [SerializeField] private GameObject waypointsContainer;
     [SerializeField] private float walkSpeed = 4;
-    [SerializeField] private float runSpeed = 6;
+    [SerializeField] private float runSpeed = 7;
     [SerializeField] private float viewRadius = 15;
     [SerializeField] private float viewAngle = 90;
     [SerializeField] private LayerMask playerMask;
     [SerializeField] private LayerMask obstacleMask;
+    [SerializeField] private GameObject eyeLights;
+    [SerializeField] private Collider punchArea;
+    [SerializeField] private float waitTime = 5.15f;
     private Animator _enemyAnimator;
     private List<Transform> waypoints;
     private NavMeshAgent _agent;
     private int _waypointIndex;
-    private Vector3 _playerLastPosition = Vector3.zero;
     private Vector3 _playerPosition;
     private Vector3 target;
     private bool choosingDestination;
@@ -28,8 +30,10 @@ public class EnemyAI : MonoBehaviour
     private bool _playerNear;
     private bool _playerCaught;
     private float _timeToRotate = 2;
-    private float _waitTime = 5.15f;
+    private float _waitTime;
     private bool _chaseFinished;
+    private bool _startOfChase;
+    private Transform player;
 
     private enum EnemyAnimatorParameters
     {
@@ -38,7 +42,8 @@ public class EnemyAI : MonoBehaviour
         Walk,
         Run,
         Punch,
-        Turn
+        Turn,
+        Agony
     }
 
     private void Start()
@@ -50,7 +55,10 @@ public class EnemyAI : MonoBehaviour
         _playerNear = false;
         _playerCaught = false;
         _chaseFinished = false;
+        _startOfChase = true;
         _agent.speed = walkSpeed;
+        _waitTime = waitTime;
+        eyeLights.SetActive(false);
         waypoints = waypointsContainer.GetComponentsInChildren<Transform>().ToList();
         waypoints.RemoveAt(0);
         choosingDestination = true;
@@ -67,13 +75,22 @@ public class EnemyAI : MonoBehaviour
         }
         else
         {
+            _startOfChase = true;
             Patrol();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            Debug.Log("___________Damage");
         }
     }
 
     private void Patrol()
     {
-        if (_chaseFinished || Vector3.Distance(transform.position, target) < 2.5f && !choosingDestination)
+        if (_chaseFinished || Vector3.Distance(transform.position, target) < _agent.stoppingDistance && !choosingDestination)
         {
             _chaseFinished = false;
             StartCoroutine(UpdateDestination());
@@ -82,9 +99,23 @@ public class EnemyAI : MonoBehaviour
 
     private void Chase()
     {
+        StartCoroutine(ChaseProcess());
+    }
+    
+    private IEnumerator ChaseProcess()
+    {
+        if (_startOfChase)
+        {
+            eyeLights.SetActive(true);
+            _enemyAnimator.SetBool(EnemyAnimatorParameters.Agony.ToString(), true);
+            _agent.isStopped = true;
+            yield return new WaitForSeconds(3.35f);
+            _enemyAnimator.SetBool(EnemyAnimatorParameters.Agony.ToString(), false);
+            _startOfChase = false;
+        }
+        _enemyAnimator.SetBool(EnemyAnimatorParameters.Punch.ToString(), false);
         _isPatrol = false;
         _playerNear = false;
-        _playerLastPosition = Vector3.zero;
  
         if (!_playerCaught)
         {
@@ -93,11 +124,18 @@ public class EnemyAI : MonoBehaviour
         }
         if (_agent.remainingDistance <= _agent.stoppingDistance)
         {
-            if (_waitTime <= 0 && !_playerCaught && Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) >= 6f)
+            if (Vector3.Distance(transform.position, player.position) < _agent.stoppingDistance + 3)
+            {
+                transform.rotation = Quaternion.LookRotation(player.position - transform.position );
+                _enemyAnimator.SetBool(EnemyAnimatorParameters.Punch.ToString(), true);
+            }
+            else if (_waitTime <= 0 && !_playerCaught && Vector3.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) >= 6f)
             {
                 _isPatrol = true;
                 _playerNear = false;
-                Move(walkSpeed);
+                _waitTime = waitTime;
+                // Move(walkSpeed);
+                eyeLights.SetActive(false);
             }
             else
             {
@@ -108,38 +146,6 @@ public class EnemyAI : MonoBehaviour
                     _chaseFinished = true;
                 }
                 _waitTime -= Time.deltaTime;
-            }
-        }
-    }
-    
-    void EnviromentView()
-    {
-        Collider[] playerInRange = Physics.OverlapSphere(transform.position, viewRadius, playerMask);
- 
-        for (int i = 0; i < playerInRange.Length; i++)
-        {
-            Transform player = playerInRange[i].transform;
-            Vector3 dirToPlayer = (player.position - transform.position).normalized;
-            if (Vector3.Angle(transform.forward, dirToPlayer) < viewAngle / 2)
-            {
-                float dstToPlayer = Vector3.Distance(transform.position, player.position);
-                if (!Physics.Raycast(transform.position, dirToPlayer, dstToPlayer, obstacleMask))
-                {
-                    _playerInViewRange = true;
-                    _isPatrol = false;
-                }
-                else
-                {
-                    _playerInViewRange = false;
-                }
-            }
-            if (Vector3.Distance(transform.position, player.position) > viewRadius)
-            {
-                _playerInViewRange = false;
-            }
-            if (_playerInViewRange)
-            {
-                _playerPosition = player.transform.position;
             }
         }
     }
@@ -167,15 +173,50 @@ public class EnemyAI : MonoBehaviour
         // Vector3 direction = target - transform.position;
         // transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(direction), .1f);
         Debug.Log(target);
+        Move(walkSpeed);
         _agent.SetDestination(target);
         yield return new WaitForSeconds(_timeToRotate);
         choosingDestination = false;
+    }
+
+    void EnviromentView()
+    {
+        Collider[] playerInRange = Physics.OverlapSphere(transform.position, viewRadius, playerMask);
+ 
+        for (int i = 0; i < playerInRange.Length; i++)
+        {
+            player = playerInRange[i].transform;
+            Vector3 dirToPlayer = (player.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, dirToPlayer) < viewAngle / 2)
+            {
+                float dstToPlayer = Vector3.Distance(transform.position, player.position);
+                if (!Physics.Raycast(transform.position, dirToPlayer, dstToPlayer, obstacleMask))
+                {
+                    _playerInViewRange = true;
+                    _isPatrol = false;
+                }
+                else
+                {
+                    _playerInViewRange = false;
+                }
+            }
+            if (Vector3.Distance(transform.position, player.position) > viewRadius)
+            {
+                _playerInViewRange = false;
+            }
+            if (_playerInViewRange)
+            {
+                _playerPosition = player.transform.position;
+            }
+        }
     }
 
     private void ChooseIdleAnimation()
     {
         float rnd = Random.Range(0, 100);
         _enemyAnimator.SetBool(EnemyAnimatorParameters.Walk.ToString(), false);
+        _enemyAnimator.SetBool(EnemyAnimatorParameters.Run.ToString(), false);
+        _enemyAnimator.SetBool(EnemyAnimatorParameters.Agony.ToString(), false);
         _enemyAnimator.SetBool(EnemyAnimatorParameters.Idle.ToString(), true);
         _enemyAnimator.SetFloat(EnemyAnimatorParameters.IdleProb.ToString(), rnd);
     }
@@ -191,17 +232,20 @@ public class EnemyAI : MonoBehaviour
     {
         _agent.isStopped = false;
         _agent.speed = speed;
-        if (_playerInViewRange || _playerNear)
+        if (_playerInViewRange || _playerNear || speed == runSpeed)
         {
             _enemyAnimator.SetBool(EnemyAnimatorParameters.Run.ToString(), true);
             _enemyAnimator.SetBool(EnemyAnimatorParameters.Walk.ToString(), false);
             _enemyAnimator.SetBool(EnemyAnimatorParameters.Idle.ToString(), false);
+            _enemyAnimator.SetBool(EnemyAnimatorParameters.Agony.ToString(), false);
+            
         }
         else
         {
             _enemyAnimator.SetBool(EnemyAnimatorParameters.Walk.ToString(), true);
             _enemyAnimator.SetBool(EnemyAnimatorParameters.Run.ToString(), false);
             _enemyAnimator.SetBool(EnemyAnimatorParameters.Idle.ToString(), false);
+            _enemyAnimator.SetBool(EnemyAnimatorParameters.Agony.ToString(), false);
         }
     }
  
